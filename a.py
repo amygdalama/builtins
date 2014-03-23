4a7c8888c9a118b0e2b02dcffc7a2d8175e30727
amygdalama <amyehanlon@gmail.com>
2014-03-23 14:26:41 -0400

    flip flopped a and b and cleaned up the code a bit

--- a.py
+++ a.py
@@ -1,9 +1,8 @@
 import __builtin__
 
-print "Before importing b"
-print __builtin__ is __builtins__
-
-import b
-
-print "In a, after importing b"
-print __builtin__ is __builtins__
+print "In a"
+print "__name__ is:", __name__
+print "__builtin__ is __builtins__:", __builtin__ is __builtins__
+print "type(__builtin__):", type(__builtin__)
+print "type(__builtins__):", type(__builtins__)
+print "__builtins__ is __builtin__.__dict__", __builtins__ is __builtin__.__dict__
--- b.py
+++ b.py
@@ -1,4 +1,14 @@
 import __builtin__
 
-print "In b"
-print __builtin__ is __builtins__
+print "In b, before importing a"
+
+# the output from this should be the same as when we ran
+# $ python a.py
+print "__name__ is:", __name__
+print "__builtin__ is __builtins__:", __builtin__ is __builtins__
+print "type(__builtin__):", type(__builtin__)
+print "type(__builtins__):", type(__builtins__)
+print "\n"
+
+import a
+# code from a will execute here
